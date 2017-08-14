22aaa386f8d4f5189306c05e836a87b79a1dfab5
Nathan Dalal <nathandalal@Nathans-MacBook-Pro.local>
2017-08-14 19:56:11 -0400

    expanded on about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const About = () => (
-	<div className="content">
-		<h5>Made by Sarina Lim</h5>
-	</div>
+	<div className="content container has-text-centered">
+    <ul style={{listStyleType: "none"}}>
+  		<li>Idea and implementation by Sarina Lim.</li>
+      <li>Translated to React by Nathan Dalal.</li>
+      <li>Powered by <i className="fa fa-envira" style={{marginTop: "8px", fontSize: "10px"}}></i>MongoDB!</li>
+	 </ul>
+  </div>
 )
 
 export default About
