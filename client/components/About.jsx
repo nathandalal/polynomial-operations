8e62d4d8d1598e1b3096bed08687fee534b3cc1d
Nathan Dalal <nathandalal@Nathans-MacBook-Pro.local>
2017-08-25 16:40:24 -0400

    final commit at mongodb :(

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
+let basicSolution = window.hljs.highlight('python', 
+"def formatPolynomial(a, b, c):\n" +
+'  print (str(a) + "x^2" + " + " + str(b) + "x" + " + " + str(c))'
+).value
+
+let advancedSolution = window.hljs.highlight('python', 
+'def getSign(number):\n' +
+'  if number > 0:\n' +
+'    return "-"\n' +
+'  return "+"\n\n' +
+'def getCoefficient(number):\n' +
+'  if number == 1:\n' +
+'    return ""\n' +
+'  elif number == -1:\n' +
+'    return "-"\n' +
+'\n' +
+'  return str(number)\n' +
+'\n' +
+'def formatPolynomial(a, b, c):\n' +
+'  a_num = getCoefficient(a)\n' +
+'  b_sign = getSign(b)\n' +
+'  b_num = getCoefficient(abs(b))\n' +
+'  c_sign = getSign(c)\n' +
+'  c_num = str(abs(c))\n\n' +
+'  print("%sx^2 %s %sx %s %s" % (a_num, b_sign, b_num, c_sign, c_num))').value
+
 const About = () => (
 	<div className="content container">
     <div className="has-text-centered">
@@ -9,10 +35,7 @@ const About = () => (
 
     <div className="content">
       <h3>The Basic Solution</h3>
-      <pre style={{whiteSpace: "pre-line"}}><code class="python">
-def formatPolynomial(a, b, c):<br/>
-&nbsp;&nbsp;print (str(a) + "x^2" + " + " + str(b) + "x" + " + " + str(c))
-      </code></pre>
+      <pre style={{whiteSpace: "pre"}}><code class="python" dangerouslySetInnerHTML={{__html: basicSolution}}/></pre>
 
       <h6>This is a function written in a programming language called <b>Python</b>.</h6>
       <h6>We have defined a function called <code>formatPolynomial</code>.</h6>
@@ -58,32 +81,10 @@ def formatPolynomial(a, b, c):<br/>
       <h3>The Advanced Solution</h3>
       <div className="columns">
         <div className="column">
-          <pre style={{whiteSpace: "pre-line"}}><code class="python">
-            def getSign(number):<br/>
-            &nbsp;&nbsp;if number > 0:<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;return "-"<br/>
-            &nbsp;&nbsp;return "+"<br/>
-            <br/>
-            def getCoefficient(number):<br/>
-            &nbsp;&nbsp;if number == 1:<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;return ""<br/>
-            &nbsp;&nbsp;elif number == -1:<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;return "-"<br/>
-            <br/>
-            &nbsp;&nbsp;return str(number)<br/>
-            <br/>
-            def formatPolynomial(a, b, c):<br/>
-            &nbsp;&nbsp;a_num = getCoefficient(a)<br/>
-            &nbsp;&nbsp;b_sign = getSign(b)<br/>
-            &nbsp;&nbsp;b_num = getCoefficient(abs(b))<br/>
-            &nbsp;&nbsp;c_sign = getSign(c)<br/>
-            &nbsp;&nbsp;c_num = str(abs(c))<br/>
-            <br/>
-            &nbsp;&nbsp;print("%sx^2 %s %sx %s %s" % (a_num, b_sign, b_num, c_sign, c_num))
-          </code></pre>
+          <pre style={{whiteSpace: "pre"}}><code class="python" dangerouslySetInnerHTML={{__html: advancedSolution}}/></pre>
         </div>
 
-        <div className="column">
+        <div className="column is-6">
           <h6>In our original solution, we had a few problems we wanted to address.</h6>
           <ul>
             <li>All numbers were connected by "+" signs, even if numbers were negative.</li>
