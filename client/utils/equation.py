0f17d66f18ad335a928550c56d248c0a501c3c13
Nathan Dalal <nathandalal@Nathans-MacBook-Pro.local>
2017-08-25 15:25:49 -0400

    added code lesson

--- client/utils/equation.py
+++ client/utils/equation.py
@@ -1,10 +1,9 @@
 def getSign(number):
   if number < 0:
     return "-"
+  return "+"
 
-  return ""
-
-def getCoefficient(number, firstCoefficient = False):
+def getCoefficient(number):
   if number == 1:
     return ""
   elif number == -1:
@@ -12,4 +11,17 @@ def getCoefficient(number, firstCoefficient = False):
 
   return str(number)
 
-def 
+def formatPolynomial(a, b, c):
+  a_number = getCoefficient(a)
+  b_sign = getSign(b)
+  b_number = getCoefficient(abs(b))
+  c_sign = getSign(c)
+  c_number = str(abs(c))
+
+  print("%sx^2 %s %sx %s %s" % (a_number, b_sign, b_number, c_sign, c_number))
+
+formatPolynomial(3, 2, 1)
+formatPolynomial(5, 1, 3)
+formatPolynomial(-1, 8, 7)
+formatPolynomial(1, -1, -5)
+formatPolynomial(-7, 2, -4)
---
+++ client/utils/equation_imperfect.py
@@ -0,0 +1,8 @@
+def formatPolynomial(a, b, c):
+  print (str(a) + "x^2" + " + " + str(b) + "x" + " + " + str(c))
+
+formatPolynomial(3, 2, 1)
+formatPolynomial(5, 1, 3)
+formatPolynomial(-1, 8, 7)
+formatPolynomial(1, -1, -5)
+formatPolynomial(-7, 2, -4)
